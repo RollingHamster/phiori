d84bcf6b6acb894d13419de9ebd89a264abc180b
Mayu Laierlence <mayuki.minano@gmail.com>
2013-12-16 17:14:24 +0900

    hotfix

--- phiori/builtins/talk.py
+++ phiori/builtins/talk.py
@@ -3,7 +3,7 @@ def _talk_secondchange(*args, **kwargs):
 	if not "talk.timer" in phiori.var:
 		phiori.var["talk.timer"] = 0
 	if not "talk.interval" in phiori.var:
-		phiori.var["talk.interval"] = int(config["talk.interval"]) + random.randint(-2, 4)
+		phiori.var["talk.interval"] = int(phiori.config["talk.interval"]) + random.randint(-2, 4)
 	if kwargs["Reference3"] == "1":
 		phiori.var["talk.timer"] += 1
 	if phiori.var["talk.timer"] >= phiori.var["talk.interval"]:
@@ -12,4 +12,4 @@ def _talk_secondchange(*args, **kwargs):
 @handle("OnTranslate")
 def _talk_translate(*args, **kwargs):
 	phiori.var["talk.timer"] = 0
-	phiori.var["talk.interval"] = int(config["talk.interval"]) + random.randint(-2, 4)
+	phiori.var["talk.interval"] = int(phiori.config["talk.interval"]) + random.randint(-2, 4)
